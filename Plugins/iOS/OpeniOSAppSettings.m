71eee9a0039feff1e02a1d3ba867d9a798bdc457
tallyon <skowronski6@gmail.com>
2024-10-16 16:58:09 +0200

    fixed openURL API deprecation

--- Plugins/iOS/OpeniOSAppSettings.m
+++ Plugins/iOS/OpeniOSAppSettings.m
@@ -3,7 +3,10 @@
 void OpenSettingsMenu() {
     if (&UIApplicationOpenSettingsURLString != NULL) {
         NSURL *appSettings = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
-        [[UIApplication sharedApplication] openURL:appSettings];
+        
+        [[UIApplication sharedApplication] openURL:appSettings
+                                                   options:@{}
+                                     completionHandler:^(BOOL success) {}];
     }
 }
 
@@ -11,10 +14,17 @@ void OpenNotificationSettingsMenu() {
     if (@available(iOS 16.0, *)){
         if (&UIApplicationOpenSettingsURLString != NULL) {
             NSURL *appSettings = [NSURL URLWithString:UIApplicationOpenNotificationSettingsURLString];
-            [[UIApplication sharedApplication] openURL:appSettings];
+
+            [[UIApplication sharedApplication] openURL:appSettings
+                                                   options:@{}
+                                     completionHandler:^(BOOL success) {}];
         }
     } else {
         NSURL *appSettings = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
-        [[UIApplication sharedApplication] openURL:appSettings];
+
+        [[UIApplication sharedApplication] openURL:appSettings
+                                                   options:@{}
+                                     completionHandler:^(BOOL success) {}];
+        
     }
 }
